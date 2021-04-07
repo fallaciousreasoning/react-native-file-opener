224ea822efae6a7ef1a86b28cb37016064c5054e
Jay Harris <jay.harris@outlook.co.nz>
2021-04-07 19:00:06 +0200

    Update gradle

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -6,7 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 import org.json.JSONArray;
 import org.json.JSONException;
