6695c6a2eb790ace36f7e8d3391502de8fc5beda
Jay Harris <jay.harris@outlook.co.nz>
2021-04-07 18:54:59 +0200

    Format document

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -26,41 +26,41 @@ import java.util.HashMap;
 
 public class FileOpener extends ReactContextBaseJavaModule {
 
-  public FileOpener(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    public FileOpener(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-    return "FileOpener";
-  }
+    @Override
+    public String getName() {
+        return "FileOpener";
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    return constants;
-  }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        return constants;
+    }
 
-  @ReactMethod
-  public void open(String fileArg, String contentType, Promise promise) throws JSONException {
-  		File file = new File(fileArg);
+    @ReactMethod
+    public void open(String fileArg, String contentType, Promise promise) throws JSONException {
+        File file = new File(fileArg);
 
-  		if (file.exists()) {
-  			try {
-          Uri path = FileProvider.getUriForFile(getReactApplicationContext(), getReactApplicationContext().getPackageName() + ".fileprovider", file);
-  				Intent intent = new Intent(Intent.ACTION_VIEW);
-  				intent.setDataAndType(path, contentType);
-          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-  				getReactApplicationContext().startActivity(intent);
+        if (file.exists()) {
+            try {
+                Uri path = FileProvider.getUriForFile(getReactApplicationContext(), getReactApplicationContext().getPackageName() + ".fileprovider", file);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(path, contentType);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                getReactApplicationContext().startActivity(intent);
 
                 promise.resolve("Open success!!");
-  			} catch (android.content.ActivityNotFoundException e) {
+            } catch (android.content.ActivityNotFoundException e) {
                 promise.reject("Open error!!");
-  			}
-  		} else {
+            }
+        } else {
             promise.reject("File not found");
-  		}
-  	}
+        }
+    }
 
 }
--- android/src/main/java/com/fileopener/FileOpenerPackage.java
+++ android/src/main/java/com/fileopener/FileOpenerPackage.java
@@ -12,19 +12,19 @@ import java.util.List;
 
 public class FileOpenerPackage implements ReactPackage {
 
-  @Override
-  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    //Registering the module.
-    return Arrays.<NativeModule>asList(new FileOpener(reactContext));
-  }
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        //Registering the module.
+        return Arrays.<NativeModule>asList(new FileOpener(reactContext));
+    }
 
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Collections.emptyList();
-  }
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
 }
